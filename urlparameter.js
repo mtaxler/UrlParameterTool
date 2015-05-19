20920a81f9902a68d45c74df4cf71fe583407506
Matthew Axler <matthew.axler@gmail.com>
2015-05-19 10:40:03 -0500

    Added ability to return all parameter keys and values as an object. Updated example

--- urlparameter.js
+++ urlparameter.js
@@ -1,10 +1,24 @@
 function GetURLParameter(urlParameter){
-    var url = window.location.search.substring(1);
-    var urlVariables = url.split('&');
-    for (var i = 0; i < urlVariables.length; i++){
-        var parameter = urlVariables[i].split('=');
-        if (parameter[0] == urlParameter){
-            return parameter[1];
-        }
-    }
+
+	var url = window.location.search.substring(1);
+	var urlVariables = url.split('&');
+	if (urlVariables[0].length > 0){
+		var allParameters = {};
+		if (!urlParameter){
+			for (var i = 0; i < urlVariables.length; i++){
+				var parameter = urlVariables[i].split('=');
+				allParameters[parameter[0]] = parameter[1];
+			}
+			return allParameters;
+		} else {
+			for (var i = 0; i < urlVariables.length; i++){
+				var parameter = urlVariables[i].split('=');
+				if (parameter[0] == urlParameter){
+					return parameter[1];
+				}
+			}
+		} 
+	} else {
+		return null;
+	}    
 }
